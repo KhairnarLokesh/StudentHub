18fded4dc212df69a09b6af4efc33257c0d8c404
Lokesh <lkhairnar580@gmail.com>
2025-12-14 15:48:21 +0530

    Initial commit

---
+++ frontend/script.js
@@ -0,0 +1,10 @@
+document.getElementById("sellBtn").addEventListener("click", function () {
+  alert("Sell product feature coming soon!");
+});
+function generateAI() {
+  document.getElementById("title").value =
+    "Second-Hand Engineering Book";
+
+  document.getElementById("description").value =
+    "Well-maintained engineering book, suitable for college students. No missing pages.";
+}
